c7b2f3c3202a93a66125f161e1fd45f219121050
Arden Butterfield <glogfogle@gmail.com>
2020-12-30 17:16:04 -0800

    tidied up the code

--- app/flask_comic.py
+++ app/flask_comic.py
@@ -5,67 +5,74 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
 app = flask.Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 # Disable cache-ing, so CSS actually updates whenever I change it.
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# Run on port 5000
+PORT_NUM = 5000
 
 app.secret_key = "Not a secret, for now"
 
-max_panels = 49
+# Number of panels in the Epic.
+MAX_PANELS = 49
 
-more_comics_list = []
-with open("more_comics.txt", "r") as more_comics_file:
-    for line in more_comics_file:
-        if line[0] != "#" and not line.isspace():
-            more_comics_list.append(line.strip())
 
 @app.route("/")
 def index():
     app.logger.debug("Sending index page")
     return flask.render_template("index.html")
 
+
 @app.route("/parasite")
 def parasite():
     app.logger.debug("Sending Parasite page")
     return flask.render_template("parasite.html")
 
+
 @app.route("/linear")
 def comic():
     return flask.redirect("/linear/0")
 
+
 @app.route("/more")
 def misc():
+    # Fetch the filenames for the comics on the More page
+    more_comics_list = []
+    with open("more_comics.txt", "r") as more_comics_file:
+        for line in more_comics_file:
+            if line[0] != "#" and not line.isspace():
+                more_comics_list.append(line.strip())
     app.logger.debug("Sending more comics page")
     return flask.render_template("more.html", images=more_comics_list,
                                  list_len=len(more_comics_list))
 
+
 @app.route("/linear/<int:panel_num>")
 def show_panel(panel_num):
 
-    if panel_num < 0 or panel_num > max_panels:
+    if panel_num < 0 or panel_num > MAX_PANELS:
         app.logger.debug(f"{panel_num} is an invalid value. Aborting.")
         flask.abort(404)
 
-    next_panel = min(panel_num + 1, max_panels)
-    prev_panel = max(panel_num - 1, 0)
-
     # We won't display prev button if first page, or next button if last page
-    first_panel = (panel_num == 0)
-    last_panel = (panel_num == max_panels)
+    is_first_panel = (panel_num == 0)
+    is_last_panel = (panel_num == MAX_PANELS)
 
     if panel_num == 0:
         image_file = "linearalgebrapanelstitle.jpg"
     else:
         image_file = f"linearalgebrapanels{panel_num}.jpg"
 
-    app.logger.debug(f"panel: {panel_num}, prev: {prev_panel}, "
-                     f"next: {next_panel}, first: {first_panel}, last: {last_panel}.")
+    app.logger.debug(f"Sending comic: panel: {panel_num}, is_first_panel: {is_first_panel}, "
+                     f"is_last_panel: {is_last_panel}.")
 
-    return flask.render_template("panel.html", image_file=image_file,
-                                 panel_num=panel_num, next_panel=next_panel, prev_panel=prev_panel,
-                                 first_panel=first_panel,last_panel=last_panel)
+    return flask.render_template("panel.html", image_file=image_file, panel_num=panel_num,
+                                 is_first_panel=is_first_panel,is_last_panel=is_last_panel)
+
 
 app.logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
-    print(f"Opening for global access on port {5000}")
-    app.run(port=5000, host="0.0.0.0")
+    print(f"Opening for global access on port {PORT_NUM}")
+    app.run(port=PORT_NUM, host="0.0.0.0")
